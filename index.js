1214e13505b9f6e4a350fbf0fd530b7de34eff2f
sshuto0117 <sshuto0117@gmail.com>
2026-02-14 11:43:31 +0900

    修正

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 function greet(){
   console.log("Hello from the main branch!");
+=======
+function greet() {
+  console.log("Hello from the local branch!");
+>>>>>>> ce0d765 (私たちの修正)
 }
