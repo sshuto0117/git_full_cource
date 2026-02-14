70be0abf8b214ae2aeabe707b80388250d2761f7
sshuto0117 <sshuto0117@gmail.com>
2026-02-14 11:49:52 +0900

    関数を呼び出した

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-function greet(){
-  console.log("Hello from the main branch!");
-=======
 function greet() {
   console.log("Hello from the local branch!");
->>>>>>> ce0d765 (私たちの修正)
 }
+
+greet();
+
